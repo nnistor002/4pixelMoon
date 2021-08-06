a7f0befbca79b6e460a3bd60c328b10e2afe6955
Natanael Nistor <NatanaelViorelNistor@Gmail.com>
2021-08-06 12:14:54 -0400

    day 3 part 1 work... forgot to push day 2

---
+++ js/scrollController.js
@@ -0,0 +1,17 @@
+// window.onscroll =function(scrollController){
+//           if(window.scrollY < 200){
+//                document.getElementById("moonSVG").setAttribute("height", (204 - window.scrollY));
+//                document.getElementById("moonSVG").setAttribute("width", (204 - window.scrollY));
+//           }
+//           if(window.scrollY > 200 && window.scrollY < 500){
+//                document.getElementById("mercurySVG").setAttribute("height", (306 - (window.scrollY - 200)));
+//                document.getElementById("mercurySVG").setAttribute("width", (306 - (window.scrollY- 200)));
+//           }else{
+//                document.getElementById("mercurySVG").setAttribute("height", (306 - (window.scrollY - 200)));
+//                document.getElementById("mercurySVG").setAttribute("width", (306 - (window.scrollY- 200)));
+//           }
+// }
+
+function moon_BTN(){
+     
+}

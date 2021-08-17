679429f4870ef378e084420a48b38bae1d8e1ec1
Natanael Nistor <NatanaelViorelNistor@Gmail.com>
2021-08-17 14:55:21 -0400

    Work more on the web page. Started the JS code and fixed some responsive code

---
+++ js/ActionController.js
@@ -0,0 +1,67 @@
+let currentArticle = "";
+
+function close_BTN(){
+     document.getElementById('articleContainer').style.visibility = "hidden";
+     document.getElementById(currentArticle).style.visibility = "hidden";
+     currentArticle = "";
+}
+
+function moon_BTN(){
+     currentArticle = "moonArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
+
+function mercury_BTN(){
+     currentArticle = "mercuryArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
+
+function mars_BTN(){
+     currentArticle = "marsArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
+
+function venus_BTN(){
+     currentArticle = "venusArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
+
+function earth_BTN(){
+     currentArticle = "earthArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
+
+function neptune_BTN(){
+     currentArticle = "neptuneArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
+
+function uranus_BTN(){
+     currentArticle = "uranusArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
+
+function saturn_BTN(){
+     currentArticle = "saturnArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
+
+function jupiter_BTN(){
+     currentArticle = "jupiterArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
+
+function sun_BTN(){
+     currentArticle = "sunArticle"
+     document.getElementById('articleContainer').style.visibility = "visible";
+     document.getElementById(currentArticle).style.visibility = "visible";
+}
